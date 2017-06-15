8efda336407172291d6daf118578e873a0c83bb5
bersling <bersling@gmail.com>
2017-06-15 11:24:47 +0200

    compile & test on server!

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 server="ubuntu@52.59.71.133"
 
-echo "Compile typescript"
-tsc
-
 echo "Remove old directory"
 ssh ${server} "rm -rf tsmean/be"
 
-echo "Upload new contents"
+echo "Upload source"
 ssh ${server} "mkdir tsmean/be"
-scp -r dist "${server}:~/tsmean/be/dist"
+scp -r src "${server}:~/tsmean/be/src"
 scp package.json "${server}:~/tsmean/be/package.json"
+scp tsconfig.json "${server}:~/tsmean/be/tsconfig.json"
 scp -r properties "${server}:~/tsmean/be/properties"
 
 echo "Install packages on server"
-ssh ${server} "sudo npm install -g node-gyp"
-ssh ${server} "cd tsmean/be && npm install --production"
+ssh ${server} "cd tsmean/be && npm install"
+
+echo "Compiling sources"
+ssh ${server} "cd tsmean/be && tsc"
 
 echo "(Re-)Start server"
 ssh ${server} "forever stop tsmean/be/dist/index.js"
