8e68b91b17220657a20850a76fff04294c31ab72
bersling <bersling@gmail.com>
2017-06-19 10:53:41 +0200

    add shebang

--- testall.sh
+++ testall.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mocha --reporter spec --timeout 15000  --compilers ts:ts-node/register 'src/**/*.test.ts'
 cd ./router-module/
 mocha --reporter spec --timeout 15000  --compilers ts:ts-node/register 'src/**/*.test.ts'
