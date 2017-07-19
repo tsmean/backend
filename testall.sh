cf63d51001f2c5b5e19c92cae3f170e2274db7cc
bersling <bersling@gmail.com>
2017-07-19 16:14:52 +0200

    also localize testall

--- testall.sh
+++ testall.sh
@@ -4,6 +4,6 @@ for modulename in auth dbadapter main mongo mysql router
 do
   cd ./${modulename}-module/
   echo "Running tests for ${modulename}-module"
-  mocha --reporter spec --timeout 15000  --compilers ts:ts-node/register 'src/**/*.test.ts'
+  $(npm bin)/mocha --reporter spec --timeout 15000  --compilers ts:ts-node/register 'src/**/*.test.ts'
   cd ..
 done
