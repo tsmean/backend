27f00195f9ce0a28626862c7b092d99c30ac04d9
bersling <bersling@gmail.com>
2017-07-23 18:35:27 +0200

    more explicit errors

--- install.ts
+++ install.ts
@@ -10,9 +10,9 @@ const spawnSyncOptions: SpawnSyncOptionsWithStringEncoding = {
  * Setup git submodules
  */
 const initSubmodules = spawnSync('git', ['submodule', 'init'], spawnSyncOptions);
-handleCommandResult(initSubmodules);
+handleCommandResult(initSubmodules, {exitOnError: true});
 const updateSubmodules = spawnSync('git', ['submodule', 'update'], spawnSyncOptions);
-handleCommandResult(updateSubmodules, {noexit: true}); // git writes to stderr even though everything is ok, so noexit
+handleCommandResult(updateSubmodules, {exitOnError: false}); // git writes to stderr even though everything is ok, so noexit
 
 /**
  * Install all modules
@@ -23,9 +23,9 @@ modules.forEach(moduleName => {
   changeToDirectory(startingDirectory);
   changeToDirectory(`${moduleName}-module`);
   const installModuleDependencies = spawnSync('npm', ['install'], spawnSyncOptions);
-  handleCommandResult(installModuleDependencies);
+  handleCommandResult(installModuleDependencies, {exitOnError: true});
   const compileModule = spawnSync('tsc', [], spawnSyncOptions);
-  handleCommandResult(compileModule);
+  handleCommandResult(compileModule, {exitOnError: true});
 
 });
 
@@ -36,10 +36,14 @@ modules.forEach(moduleName => {
 function handleCommandResult(result: SpawnSyncReturns<string>, options?: HandleCommandResultOptions) {
   if (result.error) {
     console.error('ERROR in process:', result.error);
-    options.noexit ? '' : process.exit();
+    if (options.exitOnError) {
+      process.exit()
+    }
   } else if (result.stderr !== undefined && result.stderr !== "") {
    console.error('stderr not empty:', result.stderr);
-    options.noexit ? '' : process.exit();
+    if (options.exitOnError) {
+      process.exit()
+    }
   } else {
     console.log(result.stdout);
   }
@@ -47,7 +51,7 @@ function handleCommandResult(result: SpawnSyncReturns<string>, options?: HandleC
 
 function changeToDirectory (dir) {
   try {
-    console.log(`Changing to directory ${dir}`)
+    console.log(`Changing to directory ${dir}`);
     process.chdir(dir);
   } catch (err) {
     console.log(`Could not change to directory ${dir}: ${err}`);
@@ -56,5 +60,5 @@ function changeToDirectory (dir) {
 }
 
 interface HandleCommandResultOptions {
-  noexit: boolean
+  exitOnError: boolean
 }
