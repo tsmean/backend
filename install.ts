a8b78ec92ed0e30eba7b58bdd575d275a5167d8b
bersling <bersling@gmail.com>
2017-07-23 17:51:30 +0200

    update tests

---
+++ install.ts
@@ -0,0 +1,54 @@
+import * as process from 'process';
+import {spawn, spawnSync, SpawnSyncOptionsWithStringEncoding, SpawnSyncReturns} from 'child_process';
+
+// declaring utf8 makes sure a string is returned (as opposed to a buffer) from spawnSync
+const spawnSyncOptions: SpawnSyncOptionsWithStringEncoding = {
+  encoding: 'utf8'
+};
+
+/**
+ * Setup git submodules
+ */
+const initSubmodules = spawnSync('git', ['submodule', 'init'], spawnSyncOptions);
+handleCommandResult(initSubmodules);
+
+
+/**
+ * Install all modules
+ */
+const startingDirectory = process.cwd();
+const modules = ['dbadapter', 'main', 'mongo', 'mysql', 'router', 'auth'];
+modules.forEach(moduleName => {
+  changeToDirectory(startingDirectory);
+  changeToDirectory(`${moduleName}-module`);
+  const installModuleDependencies = spawnSync('npm', ['install'], spawnSyncOptions);
+  handleCommandResult(installModuleDependencies);
+  const compileModule = spawnSync('tsc', [], spawnSyncOptions);
+  handleCommandResult(compileModule);
+
+});
+
+
+/**
+ * Helper functions
+ */
+function handleCommandResult(result: SpawnSyncReturns<string>) {
+  if (result.error) {
+    console.error('ERROR in process:', result.error);
+    process.exit();
+  } else if (result.stderr !== undefined && result.stderr !== "") {
+   console.error('stderr not empty:', result.stderr);
+   process.exit();
+  } else {
+    console.log(result.stdout);
+  }
+}
+
+function changeToDirectory (dir) {
+  try {
+    process.chdir(dir);
+  } catch (err) {
+    console.log(`Could not change to directory ${dir}: ${err}`);
+    process.exit();
+  }
+}
--- testall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for modulename in auth dbadapter main mongo mysql router
-do
-  cd ./${modulename}-module/
-  echo "Running tests for ${modulename}-module"
-  $(npm bin)/mocha --reporter spec --timeout 15000  --compilers ts:ts-node/register 'src/**/*.test.ts'
-  cd ..
-done
